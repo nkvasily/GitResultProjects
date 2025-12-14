e12ba2ee5421ceefeabbc05641d21c852631f0a1
nkvasily <nkvasily@gmail.com>
2025-12-15 00:27:47 +0700

    Добавлен проект: vite-react_4forms_t2

---
+++ vite-react_4forms_t2/src/App.jsx
@@ -0,0 +1,13 @@
+// 'src/App.jsx'
+import CommentsSection from './components/CommentsSection.jsx';
+import styles from './App.module.css'
+
+export function App() {
+	return (
+		<>
+			<CommentsSection className={styles.app} />
+
+		</>
+	);
+}
+
